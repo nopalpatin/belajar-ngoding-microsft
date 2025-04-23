7270ce22ac2b64937a87e52b11082fbd9049081a
natchaals <naufalfathinprabowo25@gmail.com>
2025-04-24 00:04:34 +0700

    first commit

---
+++ python/main.py
@@ -0,0 +1,87 @@
+print("Hello, World!")
+a = 1
+print(a)
+
+# variable adalah tempat untuk menyimpan data
+a = 1
+b = 2
+c = 3
+
+print("nilai a b c =",a, b, c)
+
+#jenis-jenis tipe data
+
+# variable dengan tipe data string
+nama = "John"
+print(nama)
+
+# variable dengan tipe data integer
+umur = 20
+print(umur)
+
+# variable dengan tipe data float
+tinggi = 170.5
+print(tinggi)
+
+# variable dengan tipe data boolean
+status = True
+print(status)
+
+status2 = False
+print(status2)
+
+# variable dengan tipe data list
+buah = ["apel", "banana", "cherry"]
+print(buah)
+
+# variable dengan tipe data tuple
+buah = ("apel", "banana", "cherry")
+print(buah)
+
+# variable dengan tipe data dictionary
+buah = {"apel": 10, "banana": 20, "cherry": 30}
+print(buah)
+
+# variable dengan tipe data set
+buah = {"apel", "banana", "cherry"}
+print(buah)
+
+# variable dengan tipe data range
+buah = range(10)
+print(buah) 
+
+# variable dengan tipe data bytes
+buah = b"Hello, World!"
+print(buah)
+
+# variable dengan tipe data bytearray
+buah = bytearray(b"Hello, World!")
+print(buah) 
+
+# variable dengan tipe data memoryview
+buah = memoryview(b"Hello, World!")
+print(buah) 
+
+# variable dengan tipe data complex
+buah = complex(1, 2)
+print(buah)  
+
+# variable dengan tipe data frozenset
+buah = frozenset({"apel", "banana", "cherry"})
+print(buah)  
+
+# variable dengan tipe data None
+buah = None
+print(buah)
+
+
+
+
+
+
+
+
+
+
+
+
