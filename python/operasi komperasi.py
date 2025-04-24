68ee5c2c9f01f4a21b444c8a31c509be98fbc415
nopalpatin <naufalthinn@gmail.com>
2025-04-24 22:47:53 +0700

    Create operasi komperasi.py

---
+++ python/operasi komperasi.py
@@ -0,0 +1,34 @@
+#operasi komparasi
+
+a = 10
+b = 20
+
+#operator == (sama dengan)
+print(a == b)
+
+#operator != (tidak sama dengan)
+print(a != b)
+
+#operator > (lebih besar dari)
+print(a > b)
+
+#operator < (lebih kecil dari)
+print(a < b)
+
+#operator >= (lebih besar sama dengan)
+print(a >= b)
+
+#operator <= (lebih kecil sama dengan)
+print(a <= b)
+
+#operator and (dan)
+print(a == b and a < b)
+
+#operator or (atau)
+print(a == b or a < b)
+
+#operator not (tidak)
+print(not a == b)
+
+#operator is (sama dengan)
+print(a is b)
