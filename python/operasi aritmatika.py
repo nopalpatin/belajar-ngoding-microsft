01954d1cc46ad4676e6285257b0553c0f9c082ce
nopalpatin <naufalthinn@gmail.com>
2025-04-24 22:19:32 +0700

    Create operasi aritmatika.py

---
+++ python/operasi aritmatika.py
@@ -0,0 +1,57 @@
+#operasi aritmatika
+
+#penjumlahan
+a = 10
+b = 20
+c = a + b
+print(c)
+
+#pengurangan
+d = 30
+e = 10
+f = d - e
+print(f)
+
+#perkalian
+g = 5
+h = 5
+i = g * h
+print(i)
+
+#pembagian
+j = 10
+k = 2
+l = j / k
+print(l)
+
+#eksponen
+m = 2
+n = 3
+o = m ** n
+print(o)
+
+#modulus
+p = 10
+q = 3
+r = p % q
+print(r)
+
+#floor division
+s = 10
+t = 3
+u = s // t
+print(u)
+
+#prioritas operasi
+#1. ()
+#2. **
+#3. * / % //
+#4. + -
+
+#contoh
+x = 10 + 20 * 30
+print(x)
+
+#jika ingin mengubah prioritas operasi
+y = (10 + 20) * 30
+print(y)
